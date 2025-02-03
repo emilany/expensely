146de51e6ef90b56207a559448c5432dcfa15309
Emi <emilanylegaspi@gmail.com>
2025-02-03 20:13:33 +1300

    defined local server for running client

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { port: 44345, host: '0.0.0.0', open: true },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
